97caaabd01e4963dadcb2a3a20d68fd81bbdd2f9
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-12-29 21:52:22 +0000

    adding bgol.sh checks

--- bgol.sh
+++ bgol.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+DEP=""
+
+if [[ -z $1 ]]; then
+DEP=deployment.zip 
+else 
+DEP=$1
+fi
+
+
 echo "Building the go binary..."
 GOOS=linux GOARCH=amd64 go build -o main main.go
 
 echo "Creating ZIP file..."
-zip deployment.zip main 
+zip $DEP  main 
+
+echo "aws s3 cp deployment.zip s3://cryptogoserverless"
+
+aws s3 cp $DEP  s3://cryptogoserverless
+
+sleep 5
 
 echo "Clearning up..."
 rm main
